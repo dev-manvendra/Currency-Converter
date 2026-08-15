a63e6673048619c821ef62c7027acd996a957e4e
Manvendra-ML <manvendrajhansi1406@gmail.com>
2026-08-15 22:31:59 +0530

    create a new react project using vite

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='bg-orange-300 text-2xl'>this is currency converter</h1>
+    </>
+  )
+}
+
+export default App
